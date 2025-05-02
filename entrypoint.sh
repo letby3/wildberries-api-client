e7b22e57a70599f9fa726dda036c663d441a1251
letby3 <fvahit2@gmail.com>
2025-05-02 13:43:04 +0300

    feat(wildberries_api_client): Added new version wildberries_api_client 0.3.x
    changed all paths to product, orders_fbs, orders_dbs etc.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,56 +2,87 @@ ROOT_DIR_PWD="`pwd`"
 SWAGGER_FILE_DIR="`pwd`/wildberries_api_client/_swagger_doc"
 SWAGGER_OUTPUT="`pwd`/wildberries_api_client"
 
+rm -rf $SWAGGER_OUTPUT/products/*
+rm -rf $SWAGGER_OUTPUT/orders_fbs/*
+rm -rf $SWAGGER_OUTPUT/orders_dbs/*
+rm -rf $SWAGGER_OUTPUT/pickup/*
+rm -rf $SWAGGER_OUTPUT/orders_fbw/*
+rm -rf $SWAGGER_OUTPUT/promotion/*
+rm -rf $SWAGGER_OUTPUT/communications/*
+rm -rf $SWAGGER_OUTPUT/tariffs/*
+rm -rf $SWAGGER_OUTPUT/analytics/*
+rm -rf $SWAGGER_OUTPUT/reports/*
+rm -rf $SWAGGER_OUTPUT/finances/*
+rm -rf $SWAGGER_OUTPUT/wbd/*
+
+wget -O "$SWAGGER_FILE_DIR/swagger_products.yaml" \
+  https://dev.wildberries.ru/yaml/ru/02-products.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
+wget -O "$SWAGGER_FILE_DIR/swagger_orders_fbs.yaml" \
+  https://dev.wildberries.ru/yaml/ru/03-orders-fbs.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
+wget -O "$SWAGGER_FILE_DIR/swagger_orders_dbs.yaml" \
+  https://dev.wildberries.ru/yaml/ru/04-orders-dbs.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
+wget -O "$SWAGGER_FILE_DIR/swagger_pickup.yaml" \
+  https://dev.wildberries.ru/yaml/ru/05-in-store-pickup.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
+wget -O "$SWAGGER_FILE_DIR/swagger_orders_fbw.yaml" \
+  https://dev.wildberries.ru/yaml/ru/06-orders-fbw.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
 wget -O "$SWAGGER_FILE_DIR/swagger_promotion.yaml" \
-  https://openapi.wildberries.ru/promotion/swagger/api/ru/swagger.yaml \
+  https://dev.wildberries.ru/yaml/ru/07-promotion.yaml \
   -T5 \
   --no-check-certificate \
   --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
 
-wget -O "$SWAGGER_FILE_DIR/swagger_content.yaml" \
-  https://openapi.wildberries.ru/content/swagger/api/ru/swagger.yaml \
-  -T5 \
-  --no-check-certificate \
-  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
-
-wget -O "$SWAGGER_FILE_DIR/swagger_prices.yaml" \
-  https://openapi.wildberries.ru/prices/swagger/api/ru/swagger.yaml \
-  -T5 \
-  --no-check-certificate \
-  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
-
-wget -O "$SWAGGER_FILE_DIR/swagger_marketplace.yaml" \
-  https://openapi.wildberries.ru/marketplace/swagger/api/ru/swagger.yaml \
-  -T5 \
-  --no-check-certificate \
-  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
-
-wget -O "$SWAGGER_FILE_DIR/swagger_statistics.yaml" \
-  https://openapi.wildberries.ru/statistics/swagger/api/ru/swagger.yaml \
-  -T5 \
-  --no-check-certificate \
-  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
-
-wget -O "$SWAGGER_FILE_DIR/swagger_analytics.yaml" \
-  https://openapi.wildberries.ru/analytics/swagger/api/ru/swagger.yaml \
-  -T5 \
-  --no-check-certificate \
-  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
-
-wget -O "$SWAGGER_FILE_DIR/swagger_recommendations.yaml" \
-  https://openapi.wildberries.ru/recommendations/swagger/api/ru/swagger.yaml \
-  -T5 \
-  --no-check-certificate \
-  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
-
-wget -O "$SWAGGER_FILE_DIR/swagger_feedbacks_questions.yaml" \
-  https://openapi.wildberries.ru/feedbacks-questions/swagger/api/ru/swagger.yaml \
+wget -O "$SWAGGER_FILE_DIR/swagger_communications.yaml" \
+  https://dev.wildberries.ru/yaml/ru/08-communications.yaml \
   -T5 \
   --no-check-certificate \
   --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
 
 wget -O "$SWAGGER_FILE_DIR/swagger_tariffs.yaml" \
-  https://openapi.wildberries.ru/tariffs/swagger/api/ru/swagger.yaml \
+  https://dev.wildberries.ru/yaml/ru/09-tariffs.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
+wget -O "$SWAGGER_FILE_DIR/swagger_analytics.yaml" \
+  https://dev.wildberries.ru/yaml/ru/10-analytics.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
+wget -O "$SWAGGER_FILE_DIR/swagger_reports.yaml" \
+  https://dev.wildberries.ru/yaml/ru/11-reports.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
+wget -O "$SWAGGER_FILE_DIR/swagger_finances.yaml" \
+  https://dev.wildberries.ru/yaml/ru/12-finances.yaml \
+  -T5 \
+  --no-check-certificate \
+  --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
+
+wget -O "$SWAGGER_FILE_DIR/swagger_wbd.yaml" \
+  https://dev.wildberries.ru/yaml/ru/13-wbd.yaml \
   -T5 \
   --no-check-certificate \
   --retry-connrefused --waitretry=10 --read-timeout=20 --timeout=15 -t 0
@@ -67,6 +98,37 @@ mvn clean package
 
 cd ./modules/swagger-codegen-cli/target
 
+
+java -jar swagger-codegen-cli.jar generate \
+    -i "$SWAGGER_FILE_DIR/swagger_products.yaml" \
+    -l python \
+    -o "$SWAGGER_OUTPUT/products_pred" \
+    --additional-properties packageName=wildberries_api_client.products
+
+java -jar swagger-codegen-cli.jar generate \
+    -i "$SWAGGER_FILE_DIR/swagger_orders_fbs.yaml" \
+    -l python \
+    -o "$SWAGGER_OUTPUT/orders_fbs_pred" \
+    --additional-properties packageName=wildberries_api_client.orders_fbs
+
+java -jar swagger-codegen-cli.jar generate \
+    -i "$SWAGGER_FILE_DIR/swagger_orders_dbs.yaml" \
+    -l python \
+    -o "$SWAGGER_OUTPUT/orders_dbs_pred" \
+    --additional-properties packageName=wildberries_api_client.orders_dbs
+
+java -jar swagger-codegen-cli.jar generate \
+    -i "$SWAGGER_FILE_DIR/swagger_pickup.yaml" \
+    -l python \
+    -o "$SWAGGER_OUTPUT/pickup_pred" \
+    --additional-properties packageName=wildberries_api_client.pickup
+
+java -jar swagger-codegen-cli.jar generate \
+    -i "$SWAGGER_FILE_DIR/swagger_orders_fbw.yaml" \
+    -l python \
+    -o "$SWAGGER_OUTPUT/orders_fbw_pred" \
+    --additional-properties packageName=wildberries_api_client.orders_fbw
+
 java -jar swagger-codegen-cli.jar generate \
     -i "$SWAGGER_FILE_DIR/swagger_promotion.yaml" \
     -l python \
@@ -74,28 +136,16 @@ java -jar swagger-codegen-cli.jar generate \
     --additional-properties packageName=wildberries_api_client.promotion
 
 java -jar swagger-codegen-cli.jar generate \
-    -i "$SWAGGER_FILE_DIR/swagger_content.yaml" \
+    -i "$SWAGGER_FILE_DIR/swagger_communications.yaml" \
     -l python \
-    -o "$SWAGGER_OUTPUT/content_pred" \
-    --additional-properties packageName=wildberries_api_client.content
+    -o "$SWAGGER_OUTPUT/communications_pred" \
+    --additional-properties packageName=wildberries_api_client.communications
 
 java -jar swagger-codegen-cli.jar generate \
-    -i "$SWAGGER_FILE_DIR/swagger_prices.yaml" \
+    -i "$SWAGGER_FILE_DIR/swagger_tariffs.yaml" \
     -l python \
-    -o "$SWAGGER_OUTPUT/prices_pred" \
-    --additional-properties packageName=wildberries_api_client.prices
-
-java -jar swagger-codegen-cli.jar generate \
-    -i "$SWAGGER_FILE_DIR/swagger_marketplace.yaml" \
-    -l python \
-    -o "$SWAGGER_OUTPUT/marketplace_pred" \
-    --additional-properties packageName=wildberries_api_client.marketplace
-
-java -jar swagger-codegen-cli.jar generate \
-    -i "$SWAGGER_FILE_DIR/swagger_statistics.yaml" \
-    -l python \
-    -o "$SWAGGER_OUTPUT/statistics_pred" \
-    --additional-properties packageName=wildberries_api_client.statistics
+    -o "$SWAGGER_OUTPUT/tariffs_pred" \
+    --additional-properties packageName=wildberries_api_client.tariffs
 
 java -jar swagger-codegen-cli.jar generate \
     -i "$SWAGGER_FILE_DIR/swagger_analytics.yaml" \
@@ -104,49 +154,58 @@ java -jar swagger-codegen-cli.jar generate \
     --additional-properties packageName=wildberries_api_client.analytics
 
 java -jar swagger-codegen-cli.jar generate \
-    -i "$SWAGGER_FILE_DIR/swagger_recommendations.yaml" \
+    -i "$SWAGGER_FILE_DIR/swagger_reports.yaml" \
     -l python \
-    -o "$SWAGGER_OUTPUT/recommendations_pred" \
-    --additional-properties packageName=wildberries_api_client.recommendations
+    -o "$SWAGGER_OUTPUT/reports_pred" \
+    --additional-properties packageName=wildberries_api_client.reports
 
 java -jar swagger-codegen-cli.jar generate \
-    -i "$SWAGGER_FILE_DIR/swagger_feedbacks_questions.yaml" \
+    -i "$SWAGGER_FILE_DIR/swagger_finances.yaml" \
     -l python \
-    -o "$SWAGGER_OUTPUT/feedbacks_questions_pred" \
-    --additional-properties packageName=wildberries_api_client.feedbacks_questions
+    -o "$SWAGGER_OUTPUT/finances_pred" \
+    --additional-properties packageName=wildberries_api_client.finances
 
 java -jar swagger-codegen-cli.jar generate \
-    -i "$SWAGGER_FILE_DIR/swagger_tariffs.yaml" \
+    -i "$SWAGGER_FILE_DIR/swagger_wbd.yaml" \
     -l python \
-    -o "$SWAGGER_OUTPUT/tariffs_pred" \
-    --additional-properties packageName=wildberries_api_client.tariffs
+    -o "$SWAGGER_OUTPUT/wbd_pred" \
+    --additional-properties packageName=wildberries_api_client.wbd
 
 cd $SWAGGER_OUTPUT
+
+mv ./products_pred/wildberries_api_client/products ./
+rm -rf ./products_pred
+
+mv ./orders_fbs_pred/wildberries_api_client/orders_fbs ./
+rm -rf ./orders_fbs_pred
+
+mv ./orders_dbs_pred/wildberries_api_client/orders_dbs ./
+rm -rf ./orders_dbs_pred
+
+mv ./pickup_pred/wildberries_api_client/pickup ./
+rm -rf ./pickup_pred
+
+mv ./orders_fbw_pred/wildberries_api_client/orders_fbw ./
+rm -rf ./orders_fbw_pred
 
 mv ./promotion_pred/wildberries_api_client/promotion ./
 rm -rf ./promotion_pred
 
-mv ./content_pred/wildberries_api_client/content ./
-rm -rf ./content_pred
+mv ./communications_pred/wildberries_api_client/communications ./
+rm -rf ./communications_pred
 
-mv ./prices_pred/wildberries_api_client/prices ./
-rm -rf ./prices_pred
-
-mv ./marketplace_pred/wildberries_api_client/marketplace ./
-rm -rf ./marketplace_pred
-
-mv ./statistics_pred/wildberries_api_client/statistics ./
-rm -rf ./statistics_pred
+mv ./tariffs_pred/wildberries_api_client/tariffs ./
+rm -rf ./tariffs_pred
 
 mv ./analytics_pred/wildberries_api_client/analytics ./
 rm -rf ./analytics_pred
 
-mv ./recommendations_pred/wildberries_api_client/recommendations ./
-rm -rf ./recommendations_pred
+mv ./reports_pred/wildberries_api_client/reports ./
+rm -rf ./reports_pred
 
-mv ./feedbacks_questions_pred/wildberries_api_client/feedbacks_questions ./
-rm -rf ./feedbacks_questions_pred
+mv ./finances_pred/wildberries_api_client/finances ./
+rm -rf ./finances_pred
 
-mv ./tariffs_pred/wildberries_api_client/tariffs ./
-rm -rf ./tariffs_pred
+mv ./wbd_pred/wildberries_api_client/wbd ./
+rm -rf ./wbd_pred
 
--- wildberries_api_client/recommendations/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from wildberries_api_client.recommendations.api.rekomendacii_api import RekomendaciiApi
